7fd6a36350efc8ec4a3743cf81897a00671239ff
王昊金 <wanghaojin@wanghaojindeMacBook-Pro.local>
2023-12-22 00:42:14 +0800

    树林密度直方统计

--- check.py
+++ check.py
@@ -1,18 +1,20 @@
 import numpy as np
-from PIL import Image
+import matplotlib.pyplot as plt
 
-image_path = 'map.jpg'
-with Image.open(image_path) as img:
-    array1 = np.array(img)
-max_val = array1.max() 
-min_val = array1.min()  
-array2 = np.load('normalized_tree_density.npy')
-print(array2.min())
-print(array2.max())
-print(array2.shape)
-check = (max_val - min_val) * array2 + min_val
-check_uint8 = check.astype(np.uint8)
-image = Image.fromarray(check_uint8)
+# Load the numpy array from the file
+npy_path = 'normalized_tree_density.npy'
+array = np.load(npy_path)
 
-path = 'check.png'
-image.save(path)
+# Create a histogram of the array values
+plt.figure(figsize=(10, 6))
+plt.hist(array.ravel(), bins=50, color='blue', alpha=0.7)  # Use ravel to flatten the array
+plt.title('Histogram of Tree Density Values')
+plt.xlabel('Normalized Density Value')
+plt.ylabel('Frequency')
+
+# Save the histogram
+histogram_path = 'histogram.png'
+plt.savefig(histogram_path)
+
+# Display the histogram
+plt.show()
